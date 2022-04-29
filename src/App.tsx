060739d5d4848cba41eb65530679f1bfa061b4b4
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2022-04-29 11:55:06 +0900

    🏷️ Install TypeScript
    - Update dependecies

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import './App.css';
+import Frame from './Page/Frame';
+
+const App = (): JSX.Element => <Frame />;
+
+export default App;
---
+++ src/Page/Frame.tsx
@@ -0,0 +1,9 @@
+const Frame = (): JSX.Element => {
+  return (
+    <div className="app">
+      <button className="btn">Hello daisyUI</button>
+    </div>
+  );
+}
+
+export default Frame;
