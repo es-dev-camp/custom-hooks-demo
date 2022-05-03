eb2ef964b42b9c822aed8129e9e5a3d11637a3c6
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2022-05-03 11:14:05 +0900

    💄 Allow opening and closing by category

--- src/Component/ReactUse/Content.tsx
+++ src/Component/ReactUse/Content.tsx
@@ -1,80 +1,12 @@
-import CardFrame from "./CardFrame";
-
-import Battery from "./Sensors/Battery";
-import Geolocation from "./Sensors/Geolocation";
-import Hover from "./Sensors/Hover";
-import Idle from "./Sensors/Idle";
-import Intersection from "./Sensors/Intersection";
-import Key from "./Sensors/Key";
-import KeyPress from "./Sensors/KeyPress";
-import KeyPressEvent from "./Sensors/KeyPressEvent";
-import Location from "./Sensors/Location";
-import LongPress from "./Sensors/LongPress";
-import MediaQuery from "./Sensors/MediaQuery";
-import MediaDevices from "./Sensors/MediaDevices";
-import Motion from "./Sensors/Motion";
-import Mouse from "./Sensors/Mouse";
-import MouseHovered from "./Sensors/MouseHovered";
-import MouseWheel from "./Sensors/MouseWheel";
-import NetworkState from "./Sensors/NetworkState";
-import Orientation from "./Sensors/Orientation";
-import PageLeave from "./Sensors/PageLeave";
-import Scratch from "./Sensors/Scratch";
-import Scroll from "./Sensors/Scroll";
-import Scrolling from "./Sensors/Scrolling";
-import StartTyping from "./Sensors/StartTyping";
-import WindowScroll from "./Sensors/WindowScroll";
-import WindowSize from "./Sensors/WindowSize";
-import Measure from "./Sensors/Measure";
-import Size from "./Sensors/Size";
-import Breakpoint from "./Sensors/Breakpoint";
-import ScrollbarWidth from "./Sensors/ScrollbarWidth";
-
-type ContentType = {
-  name: string;
-  body: () => JSX.Element;
-};
+import Sensors from "./Sensors/Category";
+import UI from "./UI/Category";
 
 const Content = (): JSX.Element => {
-  const contents: ContentType[] = [
-    { name: 'useBattery', body: Battery },
-    { name: 'useGeolocation', body: Geolocation },
-    { name: 'useHover', body: Hover },
-    { name: 'useIdle', body: Idle },
-    { name: 'useIntersection', body: Intersection },
-    { name: 'useKey', body: Key },
-    { name: 'useKeyPress', body: KeyPress },
-    { name: 'useKeyPressEvent', body: KeyPressEvent },
-    { name: 'useLocation', body: Location },
-    { name: 'useLongPress', body: LongPress },
-    { name: 'useMedia', body: MediaQuery },
-    { name: 'useMediaDevices', body: MediaDevices },
-    { name: 'useMotion', body: Motion },
-    { name: 'useMouse', body: Mouse },
-    { name: 'useMouse', body: MouseHovered },
-    { name: 'useMouseWheel', body: MouseWheel },
-    { name: 'useNetworkState', body: NetworkState },
-    { name: 'useOrientation', body: Orientation },
-    { name: 'usePageLeave', body: PageLeave },
-    { name: 'useScratch', body: Scratch },
-    { name: 'useScroll', body: Scroll },
-    { name: 'useScrolling', body: Scrolling },
-    { name: 'useStartTyping', body: StartTyping },
-    { name: 'useWindowScroll', body: WindowScroll },
-    { name: 'useWindowSize', body: WindowSize },
-    { name: 'useMeasure', body: Measure },
-    { name: 'useSize', body: Size },
-    { name: 'createBreakpoint', body: Breakpoint },
-    { name: 'useScrollbarWidth', body: ScrollbarWidth },
-  ];
   return (
-    <div className="flex flex-row flex-wrap items-start justify-start">
-      {contents.map(content => {
-        return (
-          <CardFrame key={content.name} feature={content.name} children={content.body} />
-        )
-      })}
-    </div>
+    <>
+      <Sensors />
+      <UI />
+    </>
   );
 };
 
---
+++ src/Component/ReactUse/Sensors/Category.tsx
@@ -0,0 +1,94 @@
+import { useToggle } from "react-use";
+
+import CardFrame from "../CardFrame";
+
+import Battery from "./Battery";
+import Geolocation from "./Geolocation";
+import Hover from "./Hover";
+import Idle from "./Idle";
+import Intersection from "./Intersection";
+import Key from "./Key";
+import KeyPress from "./KeyPress";
+import KeyPressEvent from "./KeyPressEvent";
+import Location from "./Location";
+import LongPress from "./LongPress";
+import MediaQuery from "./MediaQuery";
+import MediaDevices from "./MediaDevices";
+import Motion from "./Motion";
+import Mouse from "./Mouse";
+import MouseHovered from "./MouseHovered";
+import MouseWheel from "./MouseWheel";
+import NetworkState from "./NetworkState";
+import Orientation from "./Orientation";
+import PageLeave from "./PageLeave";
+import Scratch from "./Scratch";
+import Scroll from "./Scroll";
+import Scrolling from "./Scrolling";
+import StartTyping from "./StartTyping";
+import WindowScroll from "./WindowScroll";
+import WindowSize from "./WindowSize";
+import Measure from "./Measure";
+import Size from "./Size";
+import Breakpoint from "./Breakpoint";
+import ScrollbarWidth from "./ScrollbarWidth";
+
+type ContentType = {
+  name: string;
+  body: () => JSX.Element;
+};
+
+const Category = (): JSX.Element => {
+  const [toggle, setToggle] = useToggle(true);
+  const contents: ContentType[] = [
+    { name: "useBattery", body: Battery },
+    { name: "useGeolocation", body: Geolocation },
+    { name: "useHover", body: Hover },
+    { name: "useIdle", body: Idle },
+    { name: "useIntersection", body: Intersection },
+    { name: "useKey", body: Key },
+    { name: "useKeyPress", body: KeyPress },
+    { name: "useKeyPressEvent", body: KeyPressEvent },
+    { name: "useLocation", body: Location },
+    { name: "useLongPress", body: LongPress },
+    { name: "useMedia", body: MediaQuery },
+    { name: "useMediaDevices", body: MediaDevices },
+    { name: "useMotion", body: Motion },
+    { name: "useMouse", body: Mouse },
+    { name: "useMouse", body: MouseHovered },
+    { name: "useMouseWheel", body: MouseWheel },
+    { name: "useNetworkState", body: NetworkState },
+    { name: "useOrientation", body: Orientation },
+    { name: "usePageLeave", body: PageLeave },
+    { name: "useScratch", body: Scratch },
+    { name: "useScroll", body: Scroll },
+    { name: "useScrolling", body: Scrolling },
+    { name: "useStartTyping", body: StartTyping },
+    { name: "useWindowScroll", body: WindowScroll },
+    { name: "useWindowSize", body: WindowSize },
+    { name: "useMeasure", body: Measure },
+    { name: "useSize", body: Size },
+    { name: "createBreakpoint", body: Breakpoint },
+    { name: "useScrollbarWidth", body: ScrollbarWidth },
+  ];
+  return (
+    <div tabIndex={0} className="collapse collapse-arrow">
+      <input type="checkbox" checked={toggle} onClick={setToggle} />
+      <div className="collapse-title text-xl font-medium">Sensors</div>
+      <div className="collapse-content">
+        <div className="flex flex-row flex-wrap items-start justify-start">
+          {contents.map((content) => {
+            return (
+              <CardFrame
+                key={content.name}
+                feature={content.name}
+                children={content.body}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
