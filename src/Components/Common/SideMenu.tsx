d70633bb1dff7729edfdf2e03fd0b013673a9cdc
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2022-05-04 00:52:59 +0900

    🐛 Fix selected state on SideMenu

--- src/Components/Common/SideMenu.tsx
+++ src/Components/Common/SideMenu.tsx
@@ -2,10 +2,19 @@ import { useNavigate } from "react-router-dom";
 
 const SideMenu = (): JSX.Element => {
   const navigate = useNavigate();
+
+  const reactUsePath = "/react-use";
+  const selectedReactUse = location.pathname === reactUsePath;
+
   return (
     <ul className="menu px-2 overflow-y-auto w-80 bg-base-100 text-base-content">
-      <li className="text-xl font-medium bg-primary rounded-lg">
-        <a onClick={() => navigate("/react-use")}>react-use</a>
+      {/* TODO: MenuItem コンポーネントに分離する */}
+      <li
+        className={`text-xl font-medium ${
+          selectedReactUse && "bg-primary"
+        } rounded-lg`}
+      >
+        <a onClick={() => navigate(reactUsePath)}>react-use</a>
       </li>
     </ul>
   );
